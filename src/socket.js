5247673afbe291bb00c8d2275ea716066a8fe05d
Rekha Kumari <rekhakumari@Rekhas-MacBook-Air.local>
2024-11-28 13:50:55 +0530

    provide the end point

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client'
 
-const URL = process.env.NODE_ENV === 'development' ? 'https://sketchbook-be.onrender.com' : 'http://localhost:3001'
+const URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://sketchbook-be.onrender.com'
 export const socket = io(URL)
